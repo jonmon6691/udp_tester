90ce6173bbdb29de6556215c78616b59445fe242
jonmon6691 <jonmon6691@gmail.com>
2020-03-15 22:42:08 -0700

    added simplicity, error handling is for the weak

--- udp.c
+++ udp.c
@@ -6,7 +6,7 @@
 int main() {
 	int fd;
 	fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	perror("Create socket");
+	perror("socket");
 
 #if 0 // Bind to device block
 	setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, "eth0", 4);
@@ -15,6 +15,7 @@ int main() {
 
 	struct sockaddr_in sock_ptr;
 	memset(&sock_ptr, 0, sizeof(struct sockaddr_in));
+
 	sock_ptr.sin_family = AF_INET;
 	sock_ptr.sin_port = htons(3000);
 #if 0 // Bind to specific local IP
@@ -24,7 +25,7 @@ int main() {
 #endif
 
 	bind(fd, (struct sockaddr *)(&sock_ptr), sizeof(struct sockaddr));
-	perror("Bind to socket");
+	perror("bind");
 
 	fd_set fds;
 	FD_ZERO(&fds);
@@ -36,26 +37,19 @@ int main() {
 	char data[MAX_DATA];
 
 	while (1) {
-		int ret;
-		ret  = select(fd + 1, &fds, NULL, NULL, NULL);
+		select(fd + 1, &fds, NULL, NULL, NULL);
+		perror("select");
+		
+		socklen_t socklen = sizeof(source);
+		int bytes = recvfrom(fd, &data, MAX_DATA, 0, &source, &socklen);
+		perror("recvfrom");
+		
+		struct sockaddr_in *sin;
+		sin = (void *)&source;
+		
+		printf("FROM: %s\n", inet_ntoa(sin->sin_addr));
 
-		if (ret > 0) { // ret is the number of fds with data (always 1)
-			socklen_t socklen = sizeof(source);
-			int bytes = recvfrom(fd, &data, MAX_DATA, 0, &source, &socklen);
-
-			perror("Receive data");
-			struct sockaddr_in *sin;
-
-			sin = (void *)&source; // I know what I'm doing
-			printf("FROM: %s\n", inet_ntoa(sin->sin_addr));
-
-			printf("DATA:\n%.*s\n\n", bytes, data);
-
-		} else if (ret == -1) { // Error
-			perror("Select");
-		} else { // timeout
-			printf("Select: timeout");
-		}
+		printf("DATA:\n%.*s\n\n", bytes, data);
 	}
 	return 0;
 }
